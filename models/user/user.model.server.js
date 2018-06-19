a1cfd37380a4fafe2a0ec819af57903977763152
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-06-19 17:19:13 -0400

    Added provided code and a few more services for register

---
+++ models/user/user.model.server.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var userSchema = require('./user.schema.server');
+var userModel = mongoose.model('UserModel', userSchema);
+
+function findUserByCredentials(credentials) {
+  console.log(credentials);
+  return userModel.findOne(credentials, {username: 1, password: 1, email: 1, firstName: 1, lastName: 1});
+}
+
+function findUserById(userId) {
+  return userModel.findById(userId);
+}
+
+function createUser(user) {
+  return userModel.create(user);
+}
+
+function findAllUsers() {
+  return userModel.find();
+}
+
+function updateUser(user) {
+  console.log(user);
+    let u = userModel.updateOne({_id: user._id}, user);
+    return u;
+}
+
+var api = {
+  createUser: createUser,
+  findAllUsers: findAllUsers,
+  findUserById: findUserById,
+  findUserByCredentials: findUserByCredentials,
+    updateUser: updateUser,
+
+};
+
+module.exports = api;
---
+++ services/user.service.server.js
@@ -0,0 +1,82 @@
+module.exports = function (app) {
+  app.get('/api/user', findAllUsers);
+  app.get('/api/user/:userId', findUserById);
+  app.post('/api/user', createUser);
+  app.get('/api/profile', profile);
+  app.post('/api/logout', logout);
+  app.post('/api/login', login);
+  app.put('/api/user', updateUser);
+  app.get('/api/user/:username/username', findByUsername);
+  // app.get('/api/user/username/:username/password/:password', findByCredentials);
+
+  var userModel = require('../models/user/user.model.server');
+
+  function login(req, res) {
+    var credentials = req.body;
+    userModel
+      .findUserByCredentials(credentials)
+      .then(function(user) {
+        req.session['currentUser'] = user;
+        res.json(user);
+      })
+  }
+
+  function logout(req, res) {
+    req.session.destroy();
+    res.send(200);
+  }
+
+  function findUserById(req, res) {
+    var id = req.params['userId'];
+    userModel.findUserById(id)
+      .then(function (user) {
+        res.json(user);
+      })
+  }
+
+  function profile(req, res) {
+    res.send(req.session['currentUser']);
+  }
+
+  function createUser(req, res) {
+    var user = req.body;
+    userModel.createUser(user)
+      .then(function (user) {
+        req.session['currentUser'] = user;
+        res.send(user);
+      })
+  }
+
+    function updateUser(req, res) {
+        var user = req.body;
+        userModel.updateUser(user)
+            .then(function () {
+                req.session['currentUser'] = user;
+                res.json(user);
+            });
+    }
+
+  function findAllUsers(req, res) {
+    userModel.findAllUsers()
+      .then(function (users) {
+        res.send(users);
+      })
+  }
+
+  function findByUsername(req, res) {
+    var username = req.params['username'];
+      userModel.findUserByCredentials(
+          {username: username})
+          .then(function (user) {
+              res.json(user);
+          })
+  }
+    // function findByCredentials(req, res) {
+    //     var username = req.params['username'];
+    //     userModel.findUserByCredentials(
+    //         {username: username})
+    //         .then(function (user) {
+    //             res.json(user);
+    //         })
+    // }
+}
